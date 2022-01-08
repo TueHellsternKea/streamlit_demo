017121dadf2e90de888540a239cb5cd3cc185cca
Tue Hellstern <tueh@kea.dk>
2022-01-08 21:52:14 +0100

    ini file

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import streamlit as st
 import requests, uuid, json
-import(config.subscription_key)
 
-st.title("Translate")
-text=st.text_area("Enter your text",value="My name is Tue Hellstern")
+import configparser
+config = configparser.ConfigParser()
+# read the configuration file
+config.read('config.ini')
+
+st.title('Translate')
+text=st.text_area('Enter your text', value='My name is Tue Hellstern')
 target_lang = st.selectbox('Enter target language', ('da', 'it', 'de', 'sv'))
 
-subscription_key = config.subscription_key
-endpoint = "https://api.cognitive.microsofttranslator.com"
-location = "northeurope"
+subscription_key = config.get('azureapi', 'subscription_key')
+endpoint = 'https://api.cognitive.microsofttranslator.com'
+location = 'northeurope'
 path = '/translate'
 constructed_url = endpoint + path
 
@@ -31,5 +35,5 @@ body = [{
 request = requests.post(constructed_url, params=params, headers=headers, json=body)
 response = request.json()
 
-if st.button("Submit"):
+if st.button('Submit'):
   st.write(response[0]['translations'][0]['text'])
